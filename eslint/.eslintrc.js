07c00b5b965e8a24c96ccf46240a1cde4fc99dc0
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-05-21 19:18:57 +0900

    console.error及びconsole.warnの使用は許可する設定を追加

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,2 +1,6 @@
 const config = require("@wordpress/eslint-plugin/configs/recommended");
+
+// `console.warn`及び`console.error`の使用を許可
+config.rules["no-console"] = ['error', { allow: ['warn', 'error'] }];
+
 module.exports = config;

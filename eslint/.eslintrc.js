139219500a172e2c2820126266f6afa567fd84ff
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-05-22 09:44:53 +0900

    .eslintrc.jsにno-else-returnをoffにする設定を追加

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,6 +1,13 @@
 const config = require( '@wordpress/eslint-plugin/configs/recommended' );
 
-// `console.warn`及び`console.error`の使用を許可
-config.rules[ 'no-console' ] = [ 'error', { allow: [ 'warn', 'error' ] } ];
+config.rules = {
+	...config.rules,
+
+	// `console.warn`及び`console.error`の使用を許可(console.logはエラー)
+	'no-console': [ 'error', { allow: [ 'warn', 'error' ] } ],
+
+	// `if`ブロック内で`return`がある場合、`else`ブロックを省略する設定を`off`に変更
+	'no-else-return': 'off',
+};
 
 module.exports = config;

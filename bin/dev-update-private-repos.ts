0ed7c285319d68c447ad9e037eefc2361272448e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-05 18:13:36 +0900

    npm run format

--- bin/dev-update-private-repos.ts
+++ bin/dev-update-private-repos.ts
@@ -4,42 +4,53 @@
 // ------------------------------------------------------------
 
 // コマンドを実行したディレクトリにあるpackage.jsonを読み込む
-import * as fs from "fs";
-import * as path from "path";
-import { spawn } from "child_process";
-
+import * as fs from 'fs';
+import * as path from 'path';
+import { spawn } from 'child_process';
 
 const cwd = process.cwd();
-const packageJsonPath = path.join(cwd, "package.json");
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+const packageJsonPath = path.join( cwd, 'package.json' );
+const packageJson = JSON.parse( fs.readFileSync( packageJsonPath, 'utf-8' ) );
 
 // githubを参照しているリポジトリ＝プライベートリポジトリ
 // 依存関係にgithub:が含まれるものを抽出
-const githubDependencies = Object.entries(packageJson.dependencies).filter(
-	([key, value]) => (value as string).startsWith("github:")
+const githubDependencies = Object.entries( packageJson.dependencies ).filter( ( [ key, value ] ) =>
+	( value as string ).startsWith( 'github:' )
 );
-const githubDevDependencies = Object.entries(packageJson.devDependencies).filter(
-	([key, value]) => (value as string).startsWith("github:")
+const githubDevDependencies = Object.entries( packageJson.devDependencies ).filter( ( [ key, value ] ) =>
+	( value as string ).startsWith( 'github:' )
 );
 
 // githubを参照しているリポジトリが無い場合は終了
-if (githubDependencies.length === 0 && githubDevDependencies.length === 0) {
-	console.log("No github dependencies");
-	process.exit(0);
+if ( githubDependencies.length === 0 && githubDevDependencies.length === 0 ) {
+	console.log( 'No github dependencies' ); // eslint-disable-line no-console
+	process.exit( 0 );
 }
 
 // githubを参照しているリポジトリをすべて削除する
-const deletePackages = [...githubDependencies, ...githubDevDependencies].map(([key, value]) => key);
-console.log("deletePackages", deletePackages);
-spawn("npm", ["uninstall", ...deletePackages], { stdio: "inherit" });
+const deletePackages = [ ...githubDependencies, ...githubDevDependencies ].map( ( [ key, value ] ) => key );
+console.log( 'deletePackages', deletePackages ); // eslint-disable-line no-console
+spawn( 'npm', [ 'uninstall', ...deletePackages ], { stdio: 'inherit' } );
 
 // dependenciesのパッケージを再インストール
-if(githubDependencies.length > 0) {
-	console.log("npm install", githubDependencies.map(([key, value]) => `${key}@${value}`));
-	spawn("npm", ["install", ...githubDependencies.map(([key, value]) => `${key}@${value}`)], { stdio: "inherit" });
+if ( githubDependencies.length > 0 ) {
+	// eslint-disable-next-line no-console
+	console.log(
+		'npm install',
+		githubDependencies.map( ( [ key, value ] ) => `${ key }@${ value }` )
+	);
+	spawn( 'npm', [ 'install', ...githubDependencies.map( ( [ key, value ] ) => `${ key }@${ value }` ) ], {
+		stdio: 'inherit',
+	} );
 }
 // devDependenciesのパッケージを再インストール
-if(githubDevDependencies.length > 0) {
-	console.log("npm install", githubDevDependencies.map(([key, value]) => `${key}@${value}`));
-	spawn("npm", ["install", ...githubDevDependencies.map(([key, value]) => `${key}@${value}`)], { stdio: "inherit" });
+if ( githubDevDependencies.length > 0 ) {
+	// eslint-disable-next-line no-console
+	console.log(
+		'npm install',
+		githubDevDependencies.map( ( [ key, value ] ) => `${ key }@${ value }` )
+	);
+	spawn( 'npm', [ 'install', ...githubDevDependencies.map( ( [ key, value ] ) => `${ key }@${ value }` ) ], {
+		stdio: 'inherit',
+	} );
 }

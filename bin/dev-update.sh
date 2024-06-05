d0265133114ced1ca86c8c7b9e3abf7c1b8536c6
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-05 19:10:06 +0900

    シェルスクリプトを削除

--- bin/dev-update.sh
+++
@@ -1,8 +0,0 @@
-# このスクリプトが格納されているディレクトリを取得
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-if [ $1 = "privateRepos" ]; then
-	npx tsx $SCRIPT_DIR/dev-update-private-repos.ts
-elif [ $1 = "devcontainer" ]; then
-	echo "TODO"
-fi

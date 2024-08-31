b240cea6b81f47364f7ba181bba42c89232e2e64
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-31 05:38:25 +0000

    DevContainer関連ファイルを別プロジェクトにコピーする処理を修正

--- .bin/deploy-devcontainer-files.ts
+++ .bin/deploy-devcontainer-files.ts
@@ -1,38 +1,20 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { deployToProjects } from './lib/deployToProjects';
 
-// プロジェクトのルートディレクトリ(このパスの中にWordPressプラグインリポジトリ等のディレクトリが存在する)
-const PROJECT_ROOT_PATH = path.join( __dirname, '..', '..' );
-const PROJECT_DIR_PREFIX = 'serendipity-';
 const SRC_FILES_ROOT_PATH = path.join( __dirname, '..', 'docker/.devcontainer' );
 
-const main = async () => {
-	// プロジェクト関連ディレクトリ一覧を取得
-	const projectDirs = getAllProjectDirs( PROJECT_ROOT_PATH, PROJECT_DIR_PREFIX );
-
+const main = () => {
 	// 上書き用ファイル一覧を取得
 	const srcFiles = getSyncFiles( SRC_FILES_ROOT_PATH );
 
-	// 各ディレクトリにファイルをコピー
-	// ※同じ名前のファイルが存在する場合のみ上書き
-	for ( const projectDir of projectDirs ) {
-		for ( const srcFile of srcFiles ) {
-			const destPath = path.join( projectDir, '.devcontainer', path.basename( srcFile ) );
-			if ( fs.existsSync( destPath ) ) {
-				fs.copyFileSync( srcFile, destPath );
-				console.log( `Copy ${ srcFile } to ${ destPath }` );
-			}
-		}
+	// コピー先プロジェクトにおける相対パス
+	const destRelativeDir = './.devcontainer';
+
+	for ( const src of srcFiles ) {
+		deployToProjects( src, destRelativeDir );
+		console.log(); // eslint-disable-line no-console
 	}
-};
-
-/** プロジェクトディレクトリ一覧を取得します。 */
-const getAllProjectDirs = ( rootPath: string, dirNamePrefix: string ) => {
-	// ディレクトリ一覧を取得
-	const dirNames = fs.readdirSync( rootPath ).filter( ( dirName ) => dirName.startsWith( dirNamePrefix ) );
-
-	// パスにして返す
-	return dirNames.map( ( dirName ) => path.join( rootPath, dirName ) );
 };
 
 /** コピー元のファイルパス一覧を取得します。 */
@@ -44,6 +26,4 @@ const getSyncFiles = ( srcRootPath: string ) => {
 	return fileNames.map( ( fileName ) => path.join( srcRootPath, fileName ) );
 };
 
-( async () => {
-	await main();
-} )();
+main();

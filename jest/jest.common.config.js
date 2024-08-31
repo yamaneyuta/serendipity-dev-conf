470b53465dda105b5fadf782059a4db12187823d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-31 05:48:45 +0000

    fix jsdon -> jsdom

--- jest/jest.common.config.js
+++ jest/jest.common.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	preset: 'ts-jest',
 
-	// 基本的にReactのテストを実施するため`jsdon`を指定(default: `node`)
+	// 基本的にReactのテストを実施するため`jsdom`を指定(default: `node`)
 	testEnvironment: 'jsdom',
 
 	// カバレッジ出力先(default: `coverage`)
